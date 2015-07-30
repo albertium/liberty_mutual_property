fd3b6ce24014a19a0809c3ad646ac5780e861406
albertium <albertium.land@gmail.com>
2015-07-30 17:14:26 -0400

    add linear model

---
+++ scripts/gini.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def gini_coef(target, pred):
+    assert len(target) == len(pred)
+    idx = np.lexsort((target, pred))
+    cum = np.cumsum(target[idx])
+    cum = cum / float(cum[-1])
+    return ((1+len(target))/2.0 - np.sum(cum)) / len(target)
+
+def normalized_gini(target, pred):
+	return gini_coef(target, pred) / gini_coef(target, target)	
+
+def xgb_gini(pred, dtrain):
+	return 'gini', normalized_gini(dtrain.get_label(), pred)
